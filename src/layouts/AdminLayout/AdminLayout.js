4156c1ba14152f544a5c924de4795360665993e2
sebastianespina <sebastianespina9@gmail.com>
2022-10-12 23:12:38 -0300

    se avanza en frontend react

---
+++ src/layouts/AdminLayout/AdminLayout.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./AdminLayout.scss";
+
+export function AdminLayout(props) {
+  const { children } = props;
+
+  return (
+    <div>
+      <p>AdminLayout</p>
+      {children}
+    </div>
+  );
+}
---
+++ src/layouts/ClientLayout/ClientLayout.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./ClientLayout.scss";
+
+export function ClientLayout(props) {
+  const { children } = props;
+
+  return (
+    <div>
+      <p>ClientLayout</p>
+
+      {children}
+    </div>
+  );
+}
